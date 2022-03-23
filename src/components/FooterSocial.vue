a595e3dac0e08e0a4f44f3cfa767a877d615e665
Mirko Moschella <shary.mirko@gmail.com>
2022-03-23 11:34:05 +0100

    bottone social

--- src/components/FooterSocial.vue
+++ src/components/FooterSocial.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="content">
       <div class="container">
-          <div class="button"></div>
+          <div class="button">SING-UP NOW!</div>
           <div class="social"></div>
       </div>
   </div>
@@ -27,4 +27,10 @@ name: 'FooterSocial'
     background-color: #303030;
     height: 7.65rem;
 }
+.button {
+    padding: .5rem;
+    border: 2px solid #0282F9;
+    color: #fff;
+}
+
 </style>
